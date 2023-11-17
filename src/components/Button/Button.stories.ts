8455dc9c4fb9f9f0a0cc658eda78ca2df5ce17f0
paulo.moncaio <paulo.muniz@rinobox.com.br>
2023-11-17 10:00:58 -0300

    aula 4

--- src/components/Button/Button.stories.ts
+++ src/components/Button/Button.stories.ts
@@ -4,20 +4,56 @@ import Button, { ButtonProps } from './Button'
 const meta: Meta<ButtonProps> = {
   title: 'Molecules/Button',
   component: Button,
-  argTypes: {}
+  argTypes: {
+    children: {
+      type: 'string'
+    },
+    disabled: {
+      type: 'boolean'
+    },
+    className: {
+      type: 'string'
+    },
+  }
 }
 
 export default meta
 
 export const Primary: StoryObj<ButtonProps> = {
   args: {
-    children: 'Butão',
+    children: 'Botão',
+  }
+}
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    disabled: true
+  }
+}
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'secondary'
+  }
+}
+export const SecondaryDisabled: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    variant: 'secondary',
+    disabled: true
   }
 }
 
 export const Violet: StoryObj<ButtonProps> = {
   args: {
-    children: 'Butão',
+    children: 'Botão',
     className: 'theme-violet'
+  }
+}
+export const VioletSecondary: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Botão',
+    className: 'theme-violet',
+    variant: 'secondary'
   }
 }
