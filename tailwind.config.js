666f663a964993b508bd6c4f4822b56e03186edf
paulo.moncaio <paulo.muniz@rinobox.com.br>
2023-11-21 10:35:58 -0300

    aula 5 - video 5 - componente Input

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,11 +42,11 @@ const config = {
       9: 'var(--spacing-4xl) /* 56px */',
     },
     fontSize: {
-      xs: 'var(--font-xs)',
-      sm: 'var(--font-sm)',
-      md: 'var(--font-md)',
-      lg: 'var(--font-lg)',
-      xl: 'var(--font-xl)',
+      xs: 'var(--text-xs)',
+      sm: 'var(--text-sm)',
+      md: 'var(--text-md)',
+      lg: 'var(--text-lg)',
+      xl: 'var(--text-xl)',
     },
     extend: {
       colors: {
@@ -59,6 +59,7 @@ const config = {
         icon: 'rgba(var(--icon), <alpha-value>)',
         outline: 'rgba(var(--outline), <alpha-value>)',
         divider: 'rgba(var(--divider), <alpha-value>)',
+        disabled: 'rgba(var(--text-disabled), <alpha-value>)',
       },
       backgroundColor: {
         light: 'rgba(var(--bg-light), <alpha-value>)',
@@ -70,8 +71,7 @@ const config = {
           primary: 'rgba(var(--text-primary), <alpha-value>)',
           secondary: 'rgba(var(--text-secondary), <alpha-value>)',
           tertiary: 'rgba(var(--text-tertiary), <alpha-value>)',
-        },
-        disabled: 'rgba(var(--text-disabled), <alpha-value>)',
+        }, 
       },
       boxShadow: {
         sm: 'var(--shadow-sm)',
